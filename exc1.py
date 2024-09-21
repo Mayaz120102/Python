abfb207397d427b460583e1af83cd7b5c7e14c39
Mayaz120102 <abrarmayaz2002@gmail.com>
2024-09-21 21:08:56 +0600

    class and obj

--- exc1.py
+++ exc1.py
@@ -1,14 +1,17 @@
+# class and objects
+
 class Person:
     def __init__(self,name,age):
         self.name = name
         self.age = age
     
-    def __str__(self):
-        return f"{self.name} {self.age}"
-
+    # def __str__(self):
+    #     return f"{self.name} {self.age}"
+    def objfunc(self):
+        print("hello " +self.name)
 # with str
-
 p1 = Person("mayaz",22)  
-# print(p1.name,p1.age) without str 
+# print(p1.name,p1.age) ---without str 
 
-print(p1)  
+# print(p1)  
+# p1.objfunc() ---object func
